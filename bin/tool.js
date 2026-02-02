c183581f93c715c25cac57b90ce84c5442bf1993
zxl20070701 <1904314465@qq.com>
2026-02-02 21:10:57 +0800

    修复录屏软件无声音问题等

--- bin/tool.js
+++ bin/tool.js
@@ -1,3 +1,72 @@
+const { networkInterfaces, platform } = require("os");
+const { exec } = require('child_process');
+
 exports.simpleUrl = function (filepath) {
     return filepath.replace(process.cwd(), './').replace(/\\/g, '/').replace(/\/\//g, '/');
 };
+
+// 网络信息
+exports.network = function () {
+
+    let infomation = {
+        IPv4: [],
+        IPv6: []
+    };
+
+    let networks = networkInterfaces();
+
+    let IPv4Had = {}, IPv6Had = {};
+
+    for (let typeName in networks) {
+        let network = networks[typeName]
+        for (let index = 0; index < network.length; index++) {
+            if (network[index].mac != "00:00:00:00:00:00") {
+                if (network[index].family == 'IPv4') {
+                    if (!IPv4Had[network[index].mac]) {
+                        infomation.IPv4.push({
+                            address: network[index].address,
+                            mac: network[index].mac
+                        });
+
+                        IPv4Had[network[index].mac] = true;
+                    }
+                } else if (network[index].family == 'IPv6') {
+                    if (!IPv6Had[network[index].mac]) {
+                        infomation.IPv6.push({
+                            address: network[index].address,
+                            mac: network[index].mac
+                        });
+
+                        IPv6Had[network[index].mac] = true;
+                    }
+                }
+            }
+        }
+    }
+
+    return infomation;
+};
+
+exports.openBrowser = function (url) {
+    const platformValue = platform();
+    let command;
+
+    switch (platformValue) {
+        case 'win32': // Windows
+            command = `start ${url}`;
+            break;
+        case 'darwin': // macOS
+            command = `open ${url}`;
+            break;
+        default: // Linux及其他
+            command = `xdg-open ${url}`;
+    }
+
+    exec(command, (error) => {
+        if (error) {
+            console.error(`无法打开浏览器: ${error}`);
+        } else {
+            // todo
+        }
+    });
+}
--- src/pages/recorder-screen/index.js
+++ src/pages/recorder-screen/index.js
@@ -16,60 +16,143 @@ export default function (obj) {
         },
         methods: {
 
-            // 开始录制
+            // 开始录制（合并屏幕音和麦克风音）
             startRecorder: function () {
                 var _this = this;
 
                 if (!this.isRun) {
                     var videoEl = this._refs.video.value;
 
-                    // 静音播放
+                    // 静音播放防止回音
                     videoEl.muted = true;
 
-                    // 获取屏幕内容
+                    // 先获取屏幕内容（可能包含系统音）
                     navigator.mediaDevices.getDisplayMedia({
                         video: true,
                         audio: true
-                    }).then(function (stream) {
-                        _this.isRun = true;
+                    }).then(function (displayStream) {
 
-                        // 视频流及时播放
-                        videoEl.srcObject = stream;
-                        videoEl.onloadedmetadata = function () {
-                            videoEl.play();
-                        };
+                        // 尝试获取麦克风流，如果失败则降级只使用屏幕流
+                        navigator.mediaDevices.getUserMedia({audio: true}).then(function (micStream) {
 
-                        // 创建一个对指定的 MediaStream 进行录制的 MediaRecorder 对象
-                        mediaRecorder = new MediaRecorder(stream, {
-                            mimeType: MediaRecorder.isTypeSupported("video/webm; codecs=vp9") ? "video/webm; codecs=vp9" : "video/webm"
-                        });
+                            _this.isRun = true;
 
-                        // 录制
-                        var chunks = [];
-                        mediaRecorder.addEventListener("dataavailable", function (event) {
-                            chunks.push(event.data);
-                        });
+                            // 创建 AudioContext 将屏幕音与麦克风混合
+                            var AudioCtx = window.AudioContext || window.webkitAudioContext;
+                            var audioCtx = new AudioCtx();
+                            var destination = audioCtx.createMediaStreamDestination();
 
-                        stream.getVideoTracks()[0].onended = function () {
-                            mediaRecorder.stop();
-                        };
+                            // 如果屏幕流包含音频，则将其接入混音
+                            if (displayStream.getAudioTracks().length) {
+                                try {
+                                    var displaySource = audioCtx.createMediaStreamSource(displayStream);
+                                    displaySource.connect(destination);
+                                } catch (e) {
+                                    console.warn('display audio connect failed', e);
+                                }
+                            }
 
-                        // 停止的时候下载
-                        mediaRecorder.addEventListener("stop", function () {
-                            var blob = new Blob(chunks, {
-                                type: chunks[0].type
+                            // 将麦克风接入混音
+                            try {
+                                var micSource = audioCtx.createMediaStreamSource(micStream);
+                                micSource.connect(destination);
+                            } catch (e) {
+                                console.warn('mic audio connect failed', e);
+                            }
+
+                            // 构造最终用于录制的流：视频来自屏幕，音频来自混合后的 destination
+                            var combinedStream = new MediaStream();
+                            var vTrack = displayStream.getVideoTracks()[0];
+                            if (vTrack) combinedStream.addTrack(vTrack);
+                            destination.stream.getAudioTracks().forEach(function (t) {
+                                combinedStream.addTrack(t);
                             });
-                            var url = URL.createObjectURL(blob);
-                            var downEl = document.createElement("a");
-                            downEl.href = url;
-                            downEl.download = "屏幕录制.webm";
-                            downEl.click();
-                            _this.isRun = false;
-                            mediaRecorder = null;
-                        });
 
-                        // 启动
-                        mediaRecorder.start();
+                            // 视频预览使用合并流（已静音）
+                            videoEl.srcObject = combinedStream;
+                            videoEl.onloadedmetadata = function () {
+                                videoEl.play();
+                            };
+
+                            // 创建 MediaRecorder 并录制
+                            mediaRecorder = new MediaRecorder(combinedStream, {
+                                mimeType: MediaRecorder.isTypeSupported("video/webm; codecs=vp9") ? "video/webm; codecs=vp9" : "video/webm"
+                            });
+
+                            var chunks = [];
+                            mediaRecorder.addEventListener("dataavailable", function (event) {
+                                if (event.data && event.data.size) chunks.push(event.data);
+                            });
+
+                            // 当屏幕停止共享时结束录制
+                            if (displayStream.getVideoTracks()[0]) {
+                                displayStream.getVideoTracks()[0].onended = function () {
+                                    if (mediaRecorder && mediaRecorder.state !== 'inactive') mediaRecorder.stop();
+                                };
+                            }
+
+                            mediaRecorder.addEventListener("stop", function () {
+                                // 停止流与释放资源
+                                try { displayStream.getTracks().forEach(function (t) { t.stop(); }); } catch (e) {}
+                                try { micStream.getTracks().forEach(function (t) { t.stop(); }); } catch (e) {}
+                                try { audioCtx.close(); } catch (e) {}
+
+                                if (!chunks.length) {
+                                    _this.isRun = false;
+                                    mediaRecorder = null;
+                                    return;
+                                }
+
+                                var blob = new Blob(chunks, { type: chunks[0].type });
+                                var url = URL.createObjectURL(blob);
+                                var downEl = document.createElement("a");
+                                downEl.href = url;
+                                downEl.download = "屏幕录制.webm";
+                                downEl.click();
+                                _this.isRun = false;
+                                mediaRecorder = null;
+                            });
+
+                            mediaRecorder.start();
+
+                        }).catch(function (micErr) {
+                            // 获取麦克风失败，降级为仅使用屏幕流（若屏幕流有音频则录到系统声）
+                            console.warn('getUserMedia(mic) failed, fallback to displayStream only', micErr);
+                            _this.isRun = true;
+
+                            videoEl.srcObject = displayStream;
+                            videoEl.onloadedmetadata = function () { videoEl.play(); };
+
+                            mediaRecorder = new MediaRecorder(displayStream, {
+                                mimeType: MediaRecorder.isTypeSupported("video/webm; codecs=vp9") ? "video/webm; codecs=vp9" : "video/webm"
+                            });
+
+                            var chunks2 = [];
+                            mediaRecorder.addEventListener("dataavailable", function (event) {
+                                if (event.data && event.data.size) chunks2.push(event.data);
+                            });
+
+                            if (displayStream.getVideoTracks()[0]) {
+                                displayStream.getVideoTracks()[0].onended = function () {
+                                    if (mediaRecorder && mediaRecorder.state !== 'inactive') mediaRecorder.stop();
+                                };
+                            }
+
+                            mediaRecorder.addEventListener("stop", function () {
+                                try { displayStream.getTracks().forEach(function (t) { t.stop(); }); } catch (e) {}
+                                if (!chunks2.length) { _this.isRun = false; mediaRecorder = null; return; }
+                                var blob = new Blob(chunks2, { type: chunks2[0].type });
+                                var url = URL.createObjectURL(blob);
+                                var downEl = document.createElement("a");
+                                downEl.href = url;
+                                downEl.download = "屏幕录制.webm";
+                                downEl.click();
+                                _this.isRun = false;
+                                mediaRecorder = null;
+                            });
+
+                            mediaRecorder.start();
+                        });
 
                     }).catch(function (event) {
                         alert("取消录制或遇到错误\n\n" + event);
